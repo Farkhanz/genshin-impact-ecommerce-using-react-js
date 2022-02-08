426e16c020c1b6c957310b21756e54d3c4f29ae3
Farkhanz <farkhanmorelix@gmail.com>
2022-02-08 20:09:37 +0700

    Create ProductList.js

---
+++ backend-side/controllers/ProductList.js
@@ -0,0 +1,60 @@
+import Product from "../models/ProductsModel.js";
+
+export const getAllProducts = async (req,res) => {
+    try {
+        const products = await Product.findAll();
+        res.json(products);
+    } catch (error) {
+        res.json({message : error.message});
+    }
+}
+
+export const getProductById = async (req,res) => {
+    try {
+        const product = await Product.findAll({
+            where:{
+                id: req.params.id
+            }
+        });
+        res.json(product[0]);
+    } catch (error) {
+        res.status(401).send('failed to get data.');
+    }
+}
+
+export const createProduct = async (req,res) => {
+    try {
+        await Product.create(req.body);
+        res.json({
+            "Notify": "Product has been created!"
+        });
+    } catch (error) {
+        res.json({message : error.message});
+    }
+}
+
+export const updateProduct = async(req, res) => {
+    try {
+        await Product.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.json({ "message": "Product has been update" });
+    } catch (error) {
+        res.json({ message: error.message });
+    }
+}
+
+export const deleteProduct = async(req, res) => {
+    try {
+        await Product.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.json({ "message": "Product has been deleted" });
+    } catch (error) {
+        res.json({ message: error.message });
+    }
+}
