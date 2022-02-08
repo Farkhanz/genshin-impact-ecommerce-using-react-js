7ef65b1425fd4c8401381da8a6ebfc66dd91b8ac
Farkhanz <farkhanmorelix@gmail.com>
2022-02-08 20:10:03 +0700

    Create index.js

---
+++ backend-side/routes/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getAllProducts, createProduct, getProductById,
+updateProduct, deleteProduct } from "../controllers/ProductList.js";
+
+const router = express.Router();
+
+// Routers for products
+router.get('/', getAllProducts);
+router.get('/:id', getProductById);
+router.post('/', createProduct);
+router.patch('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+
+export default router;
